2af80bbc51f4d057265761606b11cce5caef20c9
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-04-05 14:35:59 +0500

    Trying to add changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,23 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        var catBean =
+                applicationContext.getBean("cat");
+        var catBean2 =
+                applicationContext.getBean("cat");
+
+
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean2);
+
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Cat {
+
+public class Cat {
+
+    private String name;
+
+
+    public Cat() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
